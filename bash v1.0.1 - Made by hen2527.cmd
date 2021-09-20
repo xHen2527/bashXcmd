2dcfc52a2477a781021205cf20333aa1c75166d5
Hen2527 <hen2527@outlook.com>
2021-09-20 08:18:11 +0800

    Add files via upload

---
+++ bash v1.0.1 - Made by hen2527.cmd
@@ -0,0 +1,16 @@
+@echo off
+title Bash ~ %Username%
+echo =========================================
+echo.
+echo Welcome to Bash!
+echo Type "help" for commands!
+echo For Command Information type "help/[command_name]"
+echo.
+echo =========================================
+echo ======================================================================================= >> "bash.log"
+echo. >> "bash.log"
+echo Logged as %USERNAME% at %DATE% %TIME% >> "bash.log"
+pause>nul|(echo Logged as %USERNAME%)
+echo.
+echo User Used Commands: >> "bash.log"
+%cd%/src/core/start.cmd
