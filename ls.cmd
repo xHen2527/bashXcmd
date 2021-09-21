a6fed04e5f6a8cd4e52b3c99b0c13e0676d2c0d3
Hen2527 <hen2527@outlook.com>
2021-09-21 11:24:33 +0800

    Delete ls.cmd

--- ls.cmd
+++
@@ -1,6 +0,0 @@
-echo =====================================================
-echo Current folder directory:
-echo %cd%
-echo =====================================================
-echo bash.utility.command.f0v - Sucessfully excuted command >> "bash.log"
-%cd%/src/core/start.cmd
