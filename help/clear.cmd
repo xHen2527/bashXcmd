e28142d4d38c5e63c684e6c9f8e50eaea9845732
Hen2527 <hen2527@outlook.com>
2021-09-21 11:25:32 +0800

    Delete clear.cmd

--- help/clear.cmd
+++
@@ -1,6 +0,0 @@
-echo ================================================
-title Help Information [clear]
-echo clear                     Used for bash to
-echo                           clear console logs
-echo ================================================
-%cd%/src/core/start.cmd
