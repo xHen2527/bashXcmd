e73b229dbfc0fb09fee59d3a2f114102f8b3e6b1
Hen2527 <hen2527@outlook.com>
2021-09-21 11:25:59 +0800

    Delete start.cmd

--- core/start.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-title Bash v1.0.1
-
-
-set /p bash="~/%Username% $ > "
-echo ~/%USERNAME% $ %bash% >> "bash.log"
-%cd%/src/%bash%.cmd
-%cd%/src/core/start.cmd
-title Bash v1.0.1 - Error Command!
