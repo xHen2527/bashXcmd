9a8117fcd93c2db05aa4a52152ac4cbc5f9a4867
Hen2527 <hen2527@outlook.com>
2021-09-21 11:23:00 +0800

    Update

---
+++ info.cmd
@@ -0,0 +1,26 @@
+echo ===================================
+echo System Information
+echo ===================================
+echo.
+echo ===================================
+echo.
+echo Your OS: %OS%
+echo Processor Achitecture: %PROCESSOR_ACHITECTURE%
+echo.
+echo Processor Identifier: %PROCESSOR_IDENTIFIER%
+echo.
+echo Processor Level: %PROCESSOR_LEVEL%
+echo.
+echo Processor Revisition: %PROCESSOR_REVISION%
+echo.
+echo Username: %USERNAME%
+echo.
+echo User Domain: %USERDOMAIN%
+echo.
+echo User Domain Roaming: %USERDOMAIN_ROAMINGPROFILE%
+echo.
+echo Session Name: %SESSIONNAME%
+echo.
+echo ====================================
+echo bash.utility.command.f0v - Sucessfully excuted command >> "bash.log"
+%cd%/src/core/start.cmd
---
+++ refresh.cmd
@@ -0,0 +1,3 @@
+powershell start-sleep 1
+echo bash.utility.command.f0v - Sucessfully excuted command >> "bash.log"
+%cd%/src/core/start.cmd
